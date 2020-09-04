1ed1986f4e34819a2e63c42db34b21a81c1130ee
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-09-04 17:04:01 +0200

    17:03

--- Tomi_tarsalgo_2020.09.04/Program.cs
+++ Tomi_tarsalgo_2020.09.04/Program.cs
@@ -3,13 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Tomi_tarsalgo_2020._09._04
 {
     class Program
     {
+        //struktúra létrehozása
+        struct Adat
+        {
+            public int ora;
+            public int perc;
+            public int azonosito;
+            public string beki;
+            public int bentlevok;
+        }
         static void Main(string[] args)
         {
+            Adat[] adatok = new Adat[1000];
+            //1. Olvassa be és tárolja el az ajto.txt fájl tartalmát!
+            StreamReader olvas = new StreamReader(@"C:\Users\Rendszergazda\Downloads\ajto.txt");
+            int n = 0;
+            int bentlevok = 0;
+            while (!olvas.EndOfStream)
+            {
+                string sor = olvas.ReadLine();
+                string[] db = sor.Split();
+                adatok[n].ora = int.Parse(db[0]);
+                adatok[n].perc = int.Parse(db[1]);
+                adatok[n].azonosito = int.Parse(db[2]);
+                adatok[n].beki = db[3];
+                if (adatok[n].beki == "be")
+                {
+                    bentlevok++;
+                }
+                else
+                {
+                    bentlevok--;
+                }
+                adatok[n].bentlevok = bentlevok;
+                n++;
+            }
+            Console.WriteLine("1. feladat: beolvasás kész!");
+            Console.WriteLine($"{adatok[7].bentlevok}");
+
+            Console.ReadKey();
         }
     }
 }
